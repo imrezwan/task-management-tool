9d4f00398058e09bd1586922ee2cdcdc8e9a6468
rezwan2525 <rezwan2525@gmail.com>
2022-11-16 23:30:49 +0600

    🎉 Implemented user authentication

--- backend/tmt_project/settings.py
+++ backend/tmt_project/settings.py
@@ -37,6 +37,35 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth',
+    'dj_rest_auth.registration',
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+SITE_ID = 1
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+
+    ],
+        
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+ALLOWED_HOSTS = [
+  'localhost',
+  '127.0.0.1',
+  '0.0.0.0',
 ]
 
 MIDDLEWARE = [
