deda839f5a63c9b6d523dfdef68fc7f46f1fc39f
rezwan2525 <rezwan2525@gmail.com>
2022-11-29 00:56:19 +0600

    Design signin component UI

---
+++ frontend/src/app/components/signin/signin.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss'],
+})
+export class SignInComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
