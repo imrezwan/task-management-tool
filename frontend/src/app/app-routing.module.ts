365c8b3b113f66ef6c81b153b04436d8d954a062
rezwan2525 <rezwan2525@gmail.com>
2022-11-29 00:52:00 +0600

    Update routing with demo route guard

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { SignInComponent } from './components/signin/signin.component';
+import { SignUpComponent } from './components/signup/signup.component';
+import { IsAuthenticated } from './services/activators';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [ IsAuthenticated ] },
+  { path: 'signin', component: SignInComponent },
+  { path: 'signup', component: SignUpComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
