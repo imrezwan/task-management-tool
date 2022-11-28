f5fbc2c6a9f6cdfa0cd753142471fdefce5ee77b
rezwan2525 <rezwan2525@gmail.com>
2022-11-29 00:53:02 +0600

    Design signup component UI

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { FormsModule } from '@angular/forms';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SignInComponent } from './components/signin/signin.component';
+import { SignUpComponent } from './components/signup/signup.component';
+import { IsAuthenticated } from './services/activators';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, SignUpComponent, SignInComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
@@ -27,8 +33,11 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
     MatIconModule,
     DragDropModule,
     MatSnackBarModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
-  providers: [],
+  providers: [IsAuthenticated],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
