d475eae0df88db1f2405621c240595c615d2cc71
rezwan2525 <rezwan2525@gmail.com>
2022-12-16 22:58:02 +0600

    BE - added API schema and docs endpoints

--- backend/tmt_project/settings.py
+++ backend/tmt_project/settings.py
@@ -61,7 +61,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny'
     ],
-        
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ]
--- backend/tmt_project/urls.py
+++ backend/tmt_project/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework.schemas import get_schema_view
+
+schema_view = get_schema_view(title="Task Management Tool")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +11,6 @@ urlpatterns = [
     path('api/v1/profile/', include('users.urls')),
     path('api/v1/rest-auth/', include('dj_rest_auth.urls')),
     path('api/v1/signup/', include('dj_rest_auth.registration.urls')),
+    path('docs/', include_docs_urls(title="Task Mangement Tool")),
+    path('schema/', schema_view),
 ]
