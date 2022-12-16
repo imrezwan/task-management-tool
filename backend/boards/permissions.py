53ffe2d1c8fa763b101b455846c87ebff66db38f
rezwan2525 <rezwan2525@gmail.com>
2022-12-16 14:44:41 +0600

    BE - update board permission

--- backend/boards/permissions.py
+++ backend/boards/permissions.py
@@ -1,7 +1,13 @@
 from rest_framework import permissions
+from .models import BoardPermission
 
 
 class IsOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
+        permissions = BoardPermission.objects.filter(board=obj)
+
+        for permission in permissions:
+            if permission.member_id == request.user.id:
+                return True
         return obj.owner.id == request.user.id
